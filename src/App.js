ebf31d80720319542a86279d52d4d45fdb0050b9
Julieta Visser <julietafvisser@gmail.com>
2021-02-15 15:47:15 -0300

    cards casi listas, falta el ícono de categoría

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import "./reset.css";
 
 function App() {
     return (
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,21 @@
+import "../styles.scss";
+
 function Card(props) {
     return (
         <div className="cardContainer">
-            <div className="imgContainer"><img src={props.pet.img}/></div>
-            <h1 className="petName">{props.pet.name}</h1>
-            <h2>{props.pet.age}</h2>
-            <p>{props.pet.description}</p>
+            <div className="imgContainer"
+                style={{
+                    backgroundImage: `url(${props.pet.img})`
+                }}
+            ></div>
+            <div className="textContainer">
+                <div className="titleContainer">
+                    <h1 className="petName">{props.pet.name}</h1>
+                    <h2 className="petAge">{props.pet.age}</h2>
+                </div>
+                <p className="petLocation">{props.pet.location}</p>
+                <p className="petDescription">{props.pet.description}</p>
+            </div>
         </div>
         );
 }
