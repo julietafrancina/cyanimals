eae725e9e1ec8893525ebfdc7628978db479353e
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-02-15 21:20:02 -0300

    Agrego páginas /visto-calle y /adopcion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar/index';
 import Home from "./pages/Home";
 import Lost from "./pages/Lost";
+import Stray from "./pages/Stray";
+import Adoption from "./pages/Adoption";
 import About from "./pages/About/About";
 import "./App.scss";
 import "./reset.css";
@@ -15,6 +17,8 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/perdidos" component={Lost} />
+                    <Route exact path="/visto-calle" component={Stray} />
+                    <Route exact path="/adopcion" component={Adoption} />
                     <Route exact path="/about" component={About} />
                 </Switch>
             </Router>
