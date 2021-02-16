50d57c393b4e180c09163fa9d3fa48027b274e7a
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-02-15 22:29:18 -0300

    Enlaces GitHub en About y Titulo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as IoIcons from "react-icons/io";
 import './About.scss';
 
 const About = () => {
@@ -21,11 +22,11 @@ const About = () => {
             <div className='authors'>
                 <figure>
                     <img src='https://avatars.githubusercontent.com/u/67765730?s=460&u=467cbea668599b9b3237e2c9a24ae05dff50e3ce&v=4' />
-                    <figcaption>Julieta Visser</figcaption>
+                    <figcaption>Julieta Visser&nbsp;&nbsp;<a href='https://github.com/julietafrancina' target='_blank' ><IoIcons.IoLogoGithub className='iconGit' /></a></figcaption>
                 </figure>
                 <figure>
                     <img src='https://avatars.githubusercontent.com/u/68615684?s=460&u=2c0abf14df135891d8caaf53531757e0b7744118&v=4' />
-                    <figcaption>Javier Rodriguez</figcaption>
+                    <figcaption>Javier Rodriguez&nbsp;&nbsp;<a href='https://github.com/JaviCeRodriguez' target='_blank' ><IoIcons.IoLogoGithub className='iconGit' /></a></figcaption>
                 </figure>
             </div>
         </section>
